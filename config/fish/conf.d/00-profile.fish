9bf879bd0fbffe831da78873730f4fad87ddde1f
Kristofers Solo <dev@kristofers.xyz>
2026-08-22 23:15:09 +0300

    Update 2026-08-22

--- config/fish/conf.d/00-profile.fish
+++ config/fish/conf.d/00-profile.fish
@@ -4,7 +4,7 @@
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
-set -gx XDG_STATE_HOME "$HOME/.local/state"
+set -gx XDG_STATE_HOME "$HOME/.local/share"
 set -gx XDG_PICTURES_DIR "$HOME/Pictures/screenshots/"
 
 # Java
@@ -106,3 +106,10 @@ set -gx SUDO_ASKPASS "$HOME/.local/bin/dmenupass"
 set -gx _JAVA_AWT_WM_NONREPARENTING 1
 
 set -gx ZDOTDIR "$XDG_CONFIG_HOME/zsh"
+
+# pnpm
+set -gx PNPM_HOME "$XDG_DATA_HOME/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
