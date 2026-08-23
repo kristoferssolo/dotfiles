ec8f8230ea4cb21ca889575a80b8c4c15f581821
Kristofers Solo <dev@kristofers.xyz>
2026-08-24 01:12:01 +0300

    feat: add unified project session picker

--- dot_config/fish/functions/fzf_sesh_connect.fish
+++ dot_config/fish/functions/fzf_sesh_connect.fish
@@ -1,22 +1,3 @@
 function fzf_sesh_connect --description "fzf-powered sesh session selector"
-    set -l session (
-        sesh list --icons | fzf-tmux -p 80%,70% \
-            --no-sort --ansi \
-            --border-label ' sesh ' \
-            --prompt '⚡  ' \
-            --header '  ^a all  ^t tmux  ^g configs  ^x zoxide  ^d tmux kill  ^f find' \
-            --bind 'tab:down,btab:up' \
-            --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
-            --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
-            --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
-            --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
-            --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
-            --bind 'ctrl-d:execute(tmux kill-session -t $(echo {} | cut -d\" \" -f2-))+change-prompt(⚡  )+reload(sesh list --icons)' \
-            --preview-window 'right:55%' \
-            --preview 'sesh preview {}'
-    )
-
-    if test -n "$session"
-        sesh connect "$session"
-    end
+    sesh-connect
 end
